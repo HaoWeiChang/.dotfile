72aaf9814494e58a0f3c2226748f51c7c50957bd
HaoWeiChang <howard3317@gmail.com>
2022-07-16 02:16:41 +0800

    new packer.lua

---
+++ nvim-lua-config/.config/nvim/lua/haoweichang/packer.lua
@@ -0,0 +1,33 @@
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/nvim-cmp")
+
+	-- For luasnip users.
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+
+
+	-- Setup telescope
+	use("nvim-lua/plenary.nvim")
+	use("nvim-telescope/telescope.nvim")
+
+
+	-- Setup treesitter
+	use("nvim-treesitter",{
+		run = ":TSUpdate"
+	})
+
+
+	-- Setup language
+	use("gruvbox-community/gruvbox")
+	use("faith/vim-go",{
+		run = ":GoUpdateBinaries"
+	})
+
+	use("TimUntersberger/neogit")
+end)
+
+
