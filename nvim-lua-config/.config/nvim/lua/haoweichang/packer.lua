c71a2357ad7b5663907eeaaaa5c58c78817bfc08
HaoWeiChang <howard3317@gmail.com>
2022-07-21 02:15:04 +0800

    add statusline func

--- nvim-lua-config/.config/nvim/lua/haoweichang/packer.lua
+++ nvim-lua-config/.config/nvim/lua/haoweichang/packer.lua
@@ -29,4 +29,8 @@ require("packer").startup(function(use)
     })
 
     use("TimUntersberger/neogit")
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
 end)
