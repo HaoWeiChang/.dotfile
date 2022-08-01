a062c8493617d3cdf9ccc486a98c1b45f595a476
HaoWeiChang <howard3317@gmail.com>
2022-08-01 23:57:57 +0800

    no command just want push

--- nvim-lua-config/.config/nvim/after/plugin/keymap/init.lua
+++ nvim-lua-config/.config/nvim/after/plugin/keymap/init.lua
@@ -8,3 +8,17 @@ inoremap("<C-c>", "<ESC>")
 
 vnoremap("J", ":m '>+1<CR>gv=gv")
 vnoremap("K", ":m '<-2<CR>gv=gv")
+
+local _filetype = {
+    ["go"] = true,
+    ["typescript"] = true,
+    ["lua"] = true
+}
+local function filekeymap()
+    if _filetype[vim.bo.filetype] then
+        print(true)
+        inoremap("[[", "[]<ESC>i")
+    end
+end
+
+filekeymap()
