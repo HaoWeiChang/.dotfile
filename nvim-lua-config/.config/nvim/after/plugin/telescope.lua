faf8079cb9d378e0e568cc898b7e4216a0a87a7a
HaoWeiChang <howard3317@gmail.com>
2022-08-14 23:59:55 +0800

    no command just want push

--- nvim-lua-config/.config/nvim/after/plugin/telescope.lua
+++ nvim-lua-config/.config/nvim/after/plugin/telescope.lua
@@ -1,13 +1,13 @@
 local Map = require("haoweichang.keymap")
 local nnoremap = Map.nnoremap
 
-nnoremap("<leader>ff", function () require('telescope.builtin').find_files() end)
-nnoremap("<leader>fg", function () require('telescope.builtin').live_grep() end)
-nnoremap("<leader>fb", function () require('telescope.builtin').buffers() end)
-nnoremap("<leader>fh", function () require('telescope.builtin').help_tags() end)
-nnoremap("<leader>fd", function () require('haoweichang.telescope').show_dotfiles() end)
+nnoremap("<leader>ff", function() require('telescope.builtin').find_files() end)
+nnoremap("<leader>fg", function() require('telescope.builtin').live_grep() end)
+nnoremap("<leader>fb", function() require('telescope.builtin').buffers() end)
+nnoremap("<leader>fh", function() require('telescope.builtin').help_tags() end)
+nnoremap("<leader>fd", function() require('haoweichang.telescope').show_dotfiles() end)
 
 
 -- git telescope
-nnoremap("<C-P>", function () require('telescope.builtin').git_branches() end)
-nnoremap("<leader>fv", function () require('telescope.builtin').git_files() end)
+nnoremap("<C-P>", function() require('telescope.builtin').git_branches() end)
+nnoremap("<leader>fv", function() require('telescope.builtin').git_files() end)
